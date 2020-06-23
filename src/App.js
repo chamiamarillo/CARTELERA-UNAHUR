07499a204c1d198c75fe645eec86733ee208a872
adramarilla <adriana.amarilla@unahur.edu.ar>
2020-06-22 23:54:07 -0300

    Tratando con los contenedores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Encabezado from  "./components/Encabezado";
-import Principal from  "./components/Principal";
-import Menu from "./components/MenuFiltroIzq"
+import MenuFiltroIzq from "./components/MenuFiltroIzq"
 
 class App extends React.Component {
   render() {
@@ -14,16 +12,13 @@ class App extends React.Component {
  
       <div class="container">
         <Encabezado/>
-        
-        <div class="row">
-          <div class="col-sm-2">
-          <Menu/>
-          </div>
-          <div class="col-sm-6" >
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.    
-          </div>
+        <div class="container1">
+
+        <MenuFiltroIzq/>
         </div>
-      </div>
+        </div>
+
+            
     );
   }
 }
--- src/components/Principal.js
+++ src/components/Principal.js
@@ -13,20 +13,7 @@ class Principal extends React.Component {
   
     return (
 
- 
-<div class="container1">
-  <MenuFiltroIzq/>
-  <p>This example demonstrates a 50%/50% split on small, medium and large devices. On extra small devices, it will stack (100% width).</p>      
-  <p>Resize the browser window to see the effect.</p>      
-  <div class="row">
-    <div class="col-sm-6">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-    </div>
-    <div class="col-sm-6" >
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.    
-    </div>
-  </div>
-</div>
+      <div>Paginal Principal </div>
     )
   }
 }
