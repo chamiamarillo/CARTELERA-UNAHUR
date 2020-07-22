becbbc5cdb2b119eab8603f5843af30b7a1951a5
marianraven <42377822+marianraven@users.noreply.github.com>
2020-07-21 22:06:14 -0300

    "repensando  componentes"

---
+++ src/components/Filtrado.js
@@ -0,0 +1,27 @@
+import { ReactComponent } from "*.svg";
+
+const debugLog = texto => console.log(azul(`[Filtrado] ${texto}`));
+
+class Filtrado extends React.Component {
+  constructor(key) {
+    this.key = key;
+    this.comision= comision;
+  }
+
+  obtenerComision(comisionBuscada) {
+    debugLog(`Buscando información ...${comisionBuscada}...`)
+      .get('http://guarani-test.unahur.edu.ar/guarani3/swagger/index.html?url=http://guarani-test.unahur.edu.ar/guarani/3.17/rest/api-docs#!/comisiones-aulas/get')
+      .query({ key: this.key, results: comisionBuscada})
+      .then(res => {
+        const comiss = res.body.results[0];
+        if (comiss.comision!== 'comision') {
+          return Promise.reject(new Error('No se encontró la comision que buscas'));
+        }
+
+        return comiss;
+      });
+  }
+ 
+}
+ 
+export default Filtrado;
