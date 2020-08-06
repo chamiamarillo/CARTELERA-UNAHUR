35c3d6f6799b8587409dd0ccef65466befc359fd
yamil <yamil.amarillo@unahur.edu.ar>
2020-08-06 09:37:30 -0300

    bien

---
+++ src/components/MostrarResultadoFiltro.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class MostrarResultadoFiltro extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentWillReceiveProps() {    
+  
+    let result = this.props.result;
+    this.setState({
+      isLoaded: true,
+      items: result
+    });
+  }
+
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>CArgando listado...</div>;
+    } else {
+      return (
+        <div>
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Codigo</th>
+                <th>Nombre</th>
+                <th>Docente</th>
+                <th>Acceso Especial</th>
+                <th>Cant. Inscriptos</th>
+                <th>Dia de Cursada</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.result.map(com => {
+                return (
+
+                  <tr key={com.comision}>
+                    <td>{com.comision}</td>
+                    <td>{com.nombre}</td>
+                    <td>{com.docentes.map(docente => docente.nombres +" "+ docente.apellido+" - ")}</td>
+                    <td>{com.acceso_especial}</td>
+                    <td>{com.cant_inscriptos}</td>
+                    <td>{com.horarios.map(hora => hora.dia+" - ")}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+
+    }
+  }
+}
+
+export default MostrarResultadoFiltro;
