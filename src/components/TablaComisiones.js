dd62075bd1b3bec594632b5caa2b4d93abfecef1
marianraven <mariangsdh@gmail.com>
2020-09-25 10:15:51 -0300

    incorporando nuevo bootstrap de 0

--- src/components/TablaComisiones.js
+++ src/components/TablaComisiones.js
@@ -10,30 +10,6 @@ import './css/TablaComisiones.css'
 
 const TablaComisiones = ({ jsonGrillaFiltrado }) => {
 
-
-  //const [jsonTabla, setJsonTabla] = useState({jsonGrillaFiltrado})
-
-  /*
-  const [checkbox, setCheckbox1] = useState(''); 
-
-  const showLogs2 = (e) => {
-    setCheckbox1(e);
-  };
-
-*/
-  /*
-    useEffect(() => {
-    
-     // console.log("FUNCION GRILLA....")
-     // console.log(grilla(jsonGrillaFiltrado))
-     setRender(mierda)
-     // setCheckbox1('')
-    }, [mierda])
-    
-  */
-    
-
-
   return (
     <>
       {
@@ -43,27 +19,19 @@ const TablaComisiones = ({ jsonGrillaFiltrado }) => {
           hover
           searchLabel='Buscar'
           entriesLabel='Cant. Registros'
-          //infoLabel
           entriesOptions={[5, 10,15,20, 25,30]}
           entries={5}
           pagesAmount={4}
           data={jsonGrillaFiltrado}
-          //checkbox
-          //headCheckboxID='id2'
-          //bodyCheckboxID='checkboxes2'
           striped
           bordered
           materialInputs
           responsive
           focus
-          //selectRow="selectHandler"
         />
       }
     </>
   );
-
-
-
 }
 
 export default TablaComisiones
