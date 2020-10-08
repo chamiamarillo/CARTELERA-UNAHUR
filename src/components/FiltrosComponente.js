42f5ef0bf835b0697c5d269a45d360f1a3988a5a
yamil <yamil.amarillo@unahur.edu.ar>
2020-10-08 02:59:36 -0300

    solucion fea a la hora de blanquear busqueda falta pulir y conbinar mejor los filtros

--- src/components/FiltrosComponente.js
+++ src/components/FiltrosComponente.js
@@ -7,7 +7,7 @@ import TablaComisiones from './TablaComisiones';
 import MaquetaGrilla from './MaquetaGrilla';
 ////
 import './css/FiltrosComponent.css'
-import { Checkbox } from 'semantic-ui-react';
+//import { Checkbox } from 'semantic-ui-react';
 
 /// ver esto para la busqueda de propuesta
 // https://material-ui.com/es/components/autocomplete/
@@ -44,9 +44,6 @@ const FiltrosComponente = () => {
       e.target.value
     ]);
   }
-
-
-
 
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -95,7 +92,25 @@ const FiltrosComponente = () => {
       setjsonGrillaFiltrado(grilla(jsonGrilla, setComisionSelec))
     }
 
-    
+    // vuelvo al estado inicial del array
+    setDias([])
+    var control1 = document.getElementById("ch001")
+    var control2 = document.getElementById("ch002")
+    var control3 = document.getElementById("ch003")
+    var control4 = document.getElementById("ch004")
+    var control5 = document.getElementById("ch005")
+    var control6 = document.getElementById("ch006")
+    //console.log("ver propiedad")
+    //console.log(control)
+   if(control1 || control2 || control3 || control4 || control5 || control6) { 
+     control1.checked=false;
+     control2.checked=false;
+     control3.checked=false;
+     control4.checked=false;
+     control5.checked=false;
+     control6.checked=false;
+    }
+   
   }
 
 
@@ -149,25 +164,24 @@ const FiltrosComponente = () => {
 
               <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                 <div class="card-body">
-                  <form  >
-                    <div className="checkbox">
-
-                      <label><input type="checkbox" value="Lunes" onChange={guardarDias}></input>Lunes</label>
+                  <form >
+                    <div className="checkbox" >
+                      <label><input type="checkbox" id="ch001" value="Lunes" onChange={guardarDias} ></input>Lunes</label>
                     </div>
                     <div className="checkbox">
-                      <label><input type="checkbox" value="Martes" onChange={guardarDias}></input>Martes</label>
+                      <label><input type="checkbox" id="ch002" value="Martes" onChange={guardarDias}></input>Martes</label>
                     </div>
                     <div className="checkbox">
-                      <label><input type="checkbox" value="Miercoles" onClick={guardarDias}></input>Miercoles</label>
+                      <label><input type="checkbox" id="ch003" value="Miercoles" onClick={guardarDias}></input>Miercoles</label>
                     </div>
                     <div className="checkbox">
-                      <label><input type="checkbox" value="Jueves" onClick={guardarDias}></input>Jueves</label>
+                      <label><input type="checkbox" id="ch004" value="Jueves" onClick={guardarDias}></input>Jueves</label>
                     </div>
                     <div className="checkbox">
-                      <label><input type="checkbox" value="Viernes" onClick={guardarDias}></input>Viernes</label>
+                      <label><input type="checkbox" id="ch005" value="Viernes" onClick={guardarDias}></input>Viernes</label>
                     </div>
                     <div className="checkbox">
-                      <label><input type="checkbox" value="Sabado" onClick={guardarDias}></input>Sabado</label>
+                      <label><input type="checkbox" id="ch006" value="Sabado" onClick={guardarDias}></input>Sabado</label>
                     </div>
                   </form>
                 </div>
