411836e44cff189fd1a84a3cdbf7b6b7b90b3712
adr <adriana.amarilla@unahur.edu.ar>
2020-12-15 02:37:11 -0300

    acomodado vista

--- src/components/MaquetaGrilla.js
+++ src/components/MaquetaGrilla.js
@@ -6,7 +6,7 @@ import './css/MaquetaGrilla.css';
 //import Edificios from "./Edificios";
 //import AulasPorEdificio from './AulasPorEdificio';
 
-const MaquetaGrilla = ({caractAulas}) => {
+const MaquetaGrilla = ({ caractAulas }) => {
 
   const [jsonEdificios, setJsonEdificos] = useState([]);
   const [jsonAulasXEdif, setJsonAulasXEdif] = useState([])
@@ -38,19 +38,18 @@ const MaquetaGrilla = ({caractAulas}) => {
 
   const mierda = auXEdif.filter(e => e.edificio === edifSelect)
 
-/*  
-  const mierda2 = mierda.filter(c => c.carac.foreach(e => 
-    {
-      edifSelect.foreach(ee => 
-        {
-          ee.id === e.ampliacion_tipo
-      })
-  }))  
-*/
+  /*  
+    const mierda2 = mierda.filter(c => c.carac.foreach(e => 
+      {
+        edifSelect.foreach(ee => 
+          {
+            ee.id === e.ampliacion_tipo
+        })
+    }))  
+  */
 
   //console.log(auXEdif)
   console.log(caractAulas)
-  
 
 
   return (
@@ -60,20 +59,16 @@ const MaquetaGrilla = ({caractAulas}) => {
 
         <div>
           <nav id="dias_asignacion_check">
-            <div className="btn-dias">
-              <button id="btn-dias"> <h4 className="btn-dias"> Lunes</h4> </button>
-              <button id="btn-dias"> <h4 className="btn-dias"> Viernes </h4> </button>
+
+            <div className="labelA"><label>Tipo de asignación: </label>
+              <div className="labelB">
+                <li ><label><input type="radio" value=""></input>Provisorio</label></li>
+                <li ><label><input type="radio" value=""></input>Permanente</label></li>
+              </div>
             </div>
 
-            <div className="labelA"><label> <h4> Tipo de asignación: </h4> </label>
-              <div className="labelB">
-                <ul>
-                  <li className="li"><label><input type="checkbox" value=""></input>Provisorio</label></li>
-                  <li className="li"><label><input type="checkbox" value=""></input>Permanente</label></li>
-                </ul></div></div>
-
-            <div>
-              <Dropdown
+            <label id="elegirEdificio" >Elegir Edificio: </label>
+              <Dropdown id="efectoEdificio"
                 placeholder='Seleccionar edificio'
                 //fluid
                 search
@@ -82,30 +77,19 @@ const MaquetaGrilla = ({caractAulas}) => {
                 onChange={(opt, meta) => setEdifSelect(meta.value)}
               />
 
-              {/*
-                sede.map(elemento => (
-                  <option key={elemento.valor} value={elemento.valor} onChange={e => setEdificacion(e.select)} >{elemento.name}</option>
-                  )
-                )
-                */}
-
-            </div>
-            <div> <p className="labelA"> Mostrar aulas con las categorías disponibles?</p>
-              <button className="btn-mostrar"> <h4> Mostrar</h4> </button></div>
-
           </nav>
         </div>
       </div>
+
       <div id="clasedeclase" >
         <div id="classSection">
           <div id="aulas_grilla1"><div> AULAS: </div>
 
             {
               mierda.map(elemento => (
-                <li className="btn-mostrarAula">{elemento.aula}</li>
+                <ul id="btn-mostrarAula">{elemento.aula}</ul>
               ))
             }
-
           </div>
         </div>
 
@@ -114,14 +98,13 @@ const MaquetaGrilla = ({caractAulas}) => {
             <p>|  07:00  |</p><p>  08: 00  |</p><p>  09:00  |</p> <p>  10:00  |</p><p> 11:00  |</p><p> 12:00  |</p><p> 13:00  |</p><p> 14:00  |</p><p> 15:00  |</p><p> 16:00  |</p>
             <p> 17:00  |</p><p> 18:00  |</p><p> 19:00  |</p><p> 20:00  |</p><p> 21:00  |</p><p> 22:00  |</p></div>
           <ol className="aulas_grilla2">
-            <li className="efectoAzul"></li>
-            <li className="efectoGris"></li>
-            <li className="efectoAzul"></li>
-            <li className="efectoGris"></li>
-            <li className="efectoAzul"></li>
+            {
+              mierda.map(elemento => (
+                <ul className="efectoAzul">{elemento.length}</ul>
+              ))
+            }
           </ol>
         </div>
-
       </div>
     </Fragment>
 
